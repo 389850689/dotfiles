1d9070a8bcf3fb19a2bdfa6c254a6956da52bea3
(\x.xx)(\x.xx) <49432296+389850689@users.noreply.github.com>
2022-05-15 15:39:58 -0700

    version 1 complete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,29 +1,3 @@
-;; Remove some GUI elements.
-(tool-bar-mode -1)   ; disable toolbar.
-(scroll-bar-mode -1) ; disable scrollbar.
-(menu-bar-mode -1)   ; disable menubar.
-; (set-fringe-mode 10) ; add some padding.
- 
-;; Show empty lines.
-; (setq-default indicate-empty-lines t)
-; (fringe-mode +1)
-; (add-hook 'prog-mode-hook 'vi-tilde-fringe-mode)
-
-;; Enable auto pairs.
-(electric-pair-mode 1)
-
-;; Switch between frames using keybinds.
-(windmove-default-keybindings)
-
-;; Enable line numbers.
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
-;; Escape key quits prompts.
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;; Set font.
-(set-face-attribute 'default nil :font "Source Code Pro" :height 110)
-
 ;; Add melpa repo to package archives.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -39,12 +13,53 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
+
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(setq inhibit-startup-message t)
+
+(tool-bar-mode -1)   ; disable toolbar.
+(scroll-bar-mode -1) ; disable scrollbar.
+(menu-bar-mode -1)   ; disable menubar.
+
+; smooth scrolling
+(setq redisplay-dont-pause t
+  scroll-margin 1
+  scroll-step 1
+  scroll-conservatively 10000
+  scroll-preserve-screen-position 1)
+
+; enable line numbers.
+(global-display-line-numbers-mode t)
+
+;; TODO: write a hook here to disable line numbers in org mode.
+
+(electric-pair-mode 1)
+
+;; install `all-the-icons` font.
+(use-package all-the-icons)
+
+(set-face-attribute 'default nil :font "Source Code Pro" :height 110)
+
+(use-package elcord
+  :init (elcord-mode)
+  :config (setq elcord-editor-icon "doom_icon"))
+
+;; Install `evil mode`, which is a vim keybind emulator.
+(use-package evil
+  :init (evil-mode 1))
+
+
+
+;; Escape key quits prompts.
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(use-package neotree
+  :init (global-set-key [f8] 'neotree-toggle))
+
 ;; Install `doom-modeline`, similar to airline from vim.
 (use-package doom-modeline
-  :ensure t
   :init (doom-modeline-mode 1))
 
 ;; Install and configure `doom-themes`
@@ -53,101 +68,102 @@
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+	doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-nord t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-tommorrow-night") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
+
+(use-package centaur-tabs
+  :demand
+  :config
+    (centaur-tabs-mode t)
+    (setq centaur-tabs-set-icons t)
+    (setq centaur-tabs-style "bar")
+    (setq centaur-tabs-height 32)
+    (setq centaur-tabs-set-modified-marker t)
+    (setq centaur-tabs-show-navigation-buttons t)
+    (setq centaur-tabs-set-bar 'left)
+    (setq centaur-tabs-gray-out-icons 'buffer)
+  :bind
+  (:map evil-normal-state-map
+    ("g t" . centaur-tabs-forward)
+    ("g T" . centaur-tabs-backward)))
+
+;; Automatically tangle our emacs.org config file when we save it
+(defun efs/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+		      (expand-file-name "~/.emacs.d/emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
 
 ;; Install and configure keybinds for `Ivy`.
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)	
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
   :init (ivy-mode 1))
 
-;; Install and configure `LSP Mode` for emacs.
-(use-package lsp-mode
-  :init
-  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  :hook ((python-mode . lsp)
-         ;; if you want which-key integration
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
 
-;; Enable hover dialogs for LSP.
-(setq lsp-ui-doc-enable t)
-;; Enable headerine.
-(setq lsp-headerline-breadcrumb-enable t)
-;; Enable symbol highlighting.
-(setq lsp-enable-symbol-highlighting t)
+(use-package swiper)
 
-;; Install company mode, for completion.
-(use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-         ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-         ("<tab>" . company-indent-or-complete-common))
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-idle-delay 0.0))
-
-(use-package company-box
-  :hook (company-mode . company-box-mode))
-
-;; Install python's jedi language server.
-(use-package lsp-jedi
-  :ensure t
+(use-package counsel
+  :bind (("C-M-j" . 'counsel-switch-buffer)
+    :map minibuffer-local-map
+    ("C-r" . 'counsel-minibuffer-history))
   :config
-  (with-eval-after-load "lsp-mode"
-    (add-to-list 'lsp-disabled-clients 'pyls)
-    (add-to-list 'lsp-enabled-clients 'jedi)))
+  (counsel-mode 1))
 
-;; Install `Emacs Python Development Enviornment`.
-(use-package elpy
-  :ensure t
-  :init
-  (elpy-enable))
+(use-package company
+  :init (global-company-mode))
 
-;; Install `evil mode`, which is a vim keybind emulator.
-(use-package evil
-  :init (evil-mode 1))
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   '(elpy vi-tilde-fringe zzz-to-char lsp-treemacs lsp-ivy lsp-ui lsp-mode ivy doom-themes elcord evil doom-modeline use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config (setq which-key-idle-delay 0.1))
+
+(use-package eglot
+  ; this'll work for now, but should have a language specific list.
+  :hook (prog-mode . eglot-ensure))
+
+(use-package solaire-mode
+  :init (setq solaire-mode-remap-fringe t) (solaire-global-mode +1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package clojure-mode)
+(use-package cider)
